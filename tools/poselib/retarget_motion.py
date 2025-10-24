d795523fcebc1137ae09132298128d8b14905d45
Weijie Wang <duangsir518@163.com>
2025-10-24 13:58:52 +0200

    add poselib

---
+++ tools/poselib/retarget_motion.py
@@ -0,0 +1,10 @@
+﻿from isaacgym.torch_utils import *
+import torch
+import json
+import numpy as np
+from poselib.core.rotation3d import *
+from poselib.skeleton.skeleton3d import SkeletonTree, SkeletonState, SkeletonMotion
+from poselib.visualization.common import plot_skeleton_state, plot_skeleton_motion_interactive
+
+if __name__ == "__main__":
+    print("Loading motion...")
