5d19d78fd856338a164b8595cd2c32bf926852fd
Weijie Wang <duangsir518@163.com>
2025-11-05 16:06:39 +0800

    Changes default logger to wandb
    Updates the default logger from tensorboard to wandb.
    
    This change improves the default metric recording setup
    to use wandb.

--- mimickit/run.py
+++ mimickit/run.py
@@ -128,7 +128,7 @@ def run(rank, num_procs, device, master_port, args):
     mode = args.parse_string("mode", "train")
     num_envs = args.parse_int("num_envs", 1)
     visualize = args.parse_bool("visualize", True)
-    logger_type = args.parse_string("logger", "tb")
+    logger_type = args.parse_string("logger", "wandb")
     log_file = args.parse_string("log_file", "output/log.txt")
     out_model_file = args.parse_string("out_model_file", "output/model.pt")
     int_output_dir = args.parse_string("int_output_dir", "")
